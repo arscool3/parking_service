b48f0885111ea5870a0ba41e371dba523bddb8c8
arscool3 <45947743+arscool3@users.noreply.github.com>
2021-12-10 11:45:30 +0600

    heroku config update

--- parking_service/settings.py
+++ parking_service/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'l@1kvlkzl#ks8o+6s)c7h(3wfg819)ooaj4b0hswdbbzxfv^ro'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
