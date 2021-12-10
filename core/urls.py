61ac149d1ed5c54c6474a0af1c7bca110e1f21ce
arscool3 <45947743+arscool3@users.noreply.github.com>
2021-12-10 11:20:07 +0600

    fix errors

--- core/urls.py
+++ core/urls.py
@@ -3,15 +3,15 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('register/<int:pk>', RegisterView.as_view()),
+    path('register/<int:pk>/', RegisterView.as_view()),
     path('login/', MyObtainTokenPairView.as_view()),
     path('places/', GetParkingPlace.as_view()),
-    path('places/<int:pk>', GetParkingTime.as_view()),
+    path('places/<int:pk>/', GetParkingTime.as_view()),
     path('add/time/', SetParkingTime.as_view()),
     path('get/time/', GetYourTime.as_view()),
-    path('delete/time/<int:pk>', DeleteTime.as_view()),
+    path('delete/time/<int:pk>/', DeleteTime.as_view()),
     path('add/place/', AddPlace.as_view()),
-    path('edit/place/<int:pk>', EditPlace.as_view()),
-    path('delete/place/<int:pk>', DeletePlace.as_view())
+    path('edit/place/<int:pk>/', EditPlace.as_view()),
+    path('delete/place/<int:pk>/', DeletePlace.as_view())
 
 ]
