4b30381668a170844e16abc1eda70e0560f3b440
arscool3 <45947743+arscool3@users.noreply.github.com>
2021-12-10 12:32:12 +0600

    edit

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser
 
 
 class ParkingTime(models.Model):
-    time = models.IntegerField(null=True)
+    time_time = models.IntegerField(null=True)
     duration = models.IntegerField(null=True)
     user_id = models.IntegerField(null=True)
     parking_place_id = models.IntegerField(null=True)
